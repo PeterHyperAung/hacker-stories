1196c5d25c60e65dc7c0bb97bfad3c055e61aa6f
PeterHyperAung <peteraungmpa2006@gmail.com>
2022-09-02 23:25:44 +0630

    react component definition

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from "react";
 import "./App.css";
+import List from "./components/List";
+import Search from "./components/Search";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark,",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-function App() {
+const App = () => {
   // const title = "React";
   const welcome = {
     greeting: "Hey",
@@ -37,28 +20,14 @@ function App() {
         <h2>
           {welcome.greeting} {getTitle()}
         </h2>
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text" />
-
+        <Search />
+        <hr />
         {/* render the list here */}
-        <ul>
-          {list.map(function (item) {
-            return (
-              <li key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-              </li>
-            );
-          })}
-        </ul>
+        <List />
         {/* and by the way: that's who you do comments in JSX */}
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,7 @@
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
+export default Search;
