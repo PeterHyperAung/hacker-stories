dddef72b0f9698b865b3a2a4320de409ba3f4ddf
PeterHyperAung <peteraungmpa2006@gmail.com>
2022-09-03 11:33:52 +0630

    handler function in JSX

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,13 @@
-const Search = () => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-  </div>
-);
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+  };
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+};
+
 export default Search;
